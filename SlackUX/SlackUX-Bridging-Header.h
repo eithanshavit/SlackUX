3d8cacbe7c239c5b4e97923bf3d6932a52576bb0
Eithan Shavit <eithanshavit@gmail.com>
2015-04-21 22:58:41 -0700

    Adding bridging header for swift

---
+++ SlackUX/SlackUX-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SlackUX-Bridging-Header.h
+//  SlackUX
+//
+//  Created by  Eithan Shavit on 4/21/15.
+//  Copyright (c) 2015 Eithan Shavit. All rights reserved.
+//
+
+#ifndef SlackUX_SlackUX_Bridging_Header_h
+#define SlackUX_SlackUX_Bridging_Header_h
+
+
+#endif
